953561b3f0f99f114bf8a1cd227890acbc0a2202
muhammadhamza2001 <muhammadhamzackt@gmail.com>
2024-06-08 03:29:30 +0500

    Done

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -9,12 +9,6 @@ import { navItems } from './NavItems';
 const Footer = () => {
     const currentYear = moment().format('YYYY');
 
-    const headings = {
-        letterSpacing: "1.2px",
-        lineHeight: "1.2",
-        padding: "20px 0",
-        color: "white"
-    };
 
 
 
@@ -54,83 +48,100 @@ const Footer = () => {
             <footer className='footer' >
                 <Container className="py-4">
                     <Row>
-                        <Col lg={4} className="text-start">
-                            <Image src={logo} width={0} height={0} style={{ width: "100%", height: "100%" }} alt='footerlogo' />
-                        </Col>
-                        <Col lg={3}>
-
-                        </Col>
-                        <Col lg={2}>
-                            <h5 style={headings}>MENU</h5>
-                            {navItems.slice(0, 5).map((page, index) => (
-                                <div className='py-2' key={page}>
-                                    <Link className="text-white" href={page.href} style={{ textDecoration: "none" }}>
-                                        {page.label}
-                                    </Link>
-                                </div>
-                            ))}
-
-                        </Col>
-                        <Col lg={3}>
-                            <h5 style={headings}>Reach Out</h5>
-                            <div className="sociallinks">
-                                {/* <Row className="pb-3">
-
-                                    {sociallinks.map((social, index) => (
-                                        <Col key={index} xs={2}>
-                                            <div className="social">
-                                                <a href={social.href} style={social}>
-                                                    {social.icon}
-                                                </a>
-                                            </div>
-                                        </Col>
-                                    ))}
-                                </Row> */}
-                                <div>
-                                    <Row className="py-2">
-                                        <Col lg={2} xs={1} className="">
-                                            <svg width="30" height="30" fill="#ffffff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M12 1.5c-4.14 0-7.5 3.024-7.5 6.75 0 6 7.5 14.25 7.5 14.25s7.5-8.25 7.5-14.25c0-3.726-3.36-6.75-7.5-6.75ZM12 12a3 3 0 1 1 0-5.999A3 3 0 0 1 12 12Z"></path>
-                                            </svg>
-                                        </Col>
-                                        <Col lg={10} xs={10}>
-                                            <div className="location text-white">
-                                                Your Address
-                                            </div>
-                                        </Col>
-                                    </Row>
-                                    <Row className="py-2">
-                                        <Col lg={2} xs={1}>
-                                            <svg width="30" height="30" fill="#ffffff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M22.45 17.35c-.244-.26-1.108-1.031-2.697-2.05-1.6-1.03-2.78-1.67-3.118-1.82a.18.18 0 0 0-.183.023 59.029 59.029 0 0 0-1.513 1.248c-.317.273-.317.273-.578.188-.457-.15-1.878-.905-3.117-2.146-1.238-1.241-2.032-2.698-2.182-3.155-.085-.26-.085-.26.188-.578.042-.05.823-.967 1.249-1.512a.18.18 0 0 0 .022-.184c-.149-.339-.79-1.518-1.819-3.118-1.02-1.588-1.79-2.451-2.05-2.696a.183.183 0 0 0-.183-.04 15.11 15.11 0 0 0-2.625 1.192 15.844 15.844 0 0 0-2.28 1.61.18.18 0 0 0-.06.176c.098.456.566 2.362 2.02 5.002 1.482 2.694 2.51 4.075 4.687 6.245 2.177 2.17 3.601 3.259 6.299 4.742 2.64 1.453 4.546 1.921 5.002 2.018a.181.181 0 0 0 .176-.059 15.83 15.83 0 0 0 1.61-2.28c.48-.835.88-1.714 1.193-2.625a.184.184 0 0 0-.04-.181Z"></path>
-                                            </svg>
-                                        </Col>
-                                        <Col lg={8} xs={10}>
-                                            <div className="location text-white">
-                                                <a style={{ textDecoration: "none", color: "white" }} href="tel:123456789">123- (456)-789</a>
-                                            </div>
-                                        </Col>
-                                    </Row>
-                                    <Row className="py-2">
-                                        <Col lg={2} xs={1}>
-                                            <svg width="30" height="30" fill="#ffffff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M21.75 3.75H2.25a.75.75 0 0 0-.75.75v15c0 .414.336.75.75.75h19.5a.75.75 0 0 0 .75-.75V4.5a.75.75 0 0 0-.75-.75ZM12 11.622 3.28 7.5h17.44L12 11.622Z"></path>
-                                            </svg>
-                                        </Col>
-                                        <Col lg={10} xs={10}>
-                                            <div className="location text-white">
-                                                <a style={{ textDecoration: "none", color: "white" }} href="mailto:isoeasy@gmail.com">isoeasy@gmail.com</a>
-                                            </div>
-                                        </Col>
-                                    </Row>
-                                </div>
+                        <Col lg={6} className="text-start">
+                            {/* <Image src={logo} width={0} height={0} style={{ width: "100%", height: "100%" }} alt='footerlogo' /> */}
+                            <div>
+                                <h1 className='footer-logo'>ISO EASY</h1>
                             </div>
                         </Col>
+
+                        <Col lg={6}>
+
+                            <div className="d-block d-lg-flex mt-2 justify-content-end">
+                                {navItems.slice(0, 5).map((page, index) => (
+                                    <div className='py-2' key={page}>
+                                        <Link className="text-white me-4" href={page.href} style={{ textDecoration: "none" }}>
+                                            {page.label}
+                                        </Link>
+                                    </div>
+                                ))}
+                            </div>
+
+
+
+                      {/* <div className="sociallinks pt-3 pt-lg-0">
+                                    <div className="justify-content-lg-end justify-content-center d-flex">
+
+                                        {sociallinks.map((social, index) => (
+                                            <div key={index} >
+                                                <div className="social">
+                                                    <Link href={social.href} style={social}>
+                                                        <span className='mx-3'>{social.icon}</span>
+                                                    </Link>
+                                                </div>
+                                            </div>
+                                        ))}
+                                    </div>
+
+                                </div> */}
+
+
+
+                        </Col>
+
                     </Row>
                 </Container>
-                <div className="footer-bottom py-4 text-center" >
+                <div className="footer-bottom py-4 " >
                     <Container>
-                        &copy; {currentYear} Iso Easy. All rights reserved.
+                        <Row>
+                            <Col xs={12} lg={6} className='order-1 order-lg-0'>
+                                <div className='text-center text-lg-start'>
+                                    &copy; {currentYear} Iso Easy. All rights reserved.
+                                </div>
+
+                            </Col>
+
+
+                            <Col xs={12} lg={6} className='order-0 order-lg-1'>
+                                <div className="justify-content-lg-end justify-content-center d-block d-lg-flex">
+                                    <div className="d-flex justify-content-center justify-content-lg-start me-4 py-2 py-lg-0">
+                                        <div>
+                                            <svg width="20" height="20" fill="#ffffff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M12 1.5c-4.14 0-7.5 3.024-7.5 6.75 0 6 7.5 14.25 7.5 14.25s7.5-8.25 7.5-14.25c0-3.726-3.36-6.75-7.5-6.75ZM12 12a3 3 0 1 1 0-5.999A3 3 0 0 1 12 12Z"></path>
+                                            </svg>
+                                        </div>
+                                        <div className="location text-white ms-2">
+                                            Your Address
+                                        </div>
+                                    </div>
+                                    <div className="d-flex justify-content-center justify-content-lg-start me-4 py-2 py-lg-0">
+                                        <div>
+                                            <svg width="20" height="20" fill="#ffffff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M22.45 17.35c-.244-.26-1.108-1.031-2.697-2.05-1.6-1.03-2.78-1.67-3.118-1.82a.18.18 0 0 0-.183.023 59.029 59.029 0 0 0-1.513 1.248c-.317.273-.317.273-.578.188-.457-.15-1.878-.905-3.117-2.146-1.238-1.241-2.032-2.698-2.182-3.155-.085-.26-.085-.26.188-.578.042-.05.823-.967 1.249-1.512a.18.18 0 0 0 .022-.184c-.149-.339-.79-1.518-1.819-3.118-1.02-1.588-1.79-2.451-2.05-2.696a.183.183 0 0 0-.183-.04 15.11 15.11 0 0 0-2.625 1.192 15.844 15.844 0 0 0-2.28 1.61.18.18 0 0 0-.06.176c.098.456.566 2.362 2.02 5.002 1.482 2.694 2.51 4.075 4.687 6.245 2.177 2.17 3.601 3.259 6.299 4.742 2.64 1.453 4.546 1.921 5.002 2.018a.181.181 0 0 0 .176-.059 15.83 15.83 0 0 0 1.61-2.28c.48-.835.88-1.714 1.193-2.625a.184.184 0 0 0-.04-.181Z"></path>
+                                            </svg>
+                                        </div>
+                                        <div className="location text-white ms-2">
+                                            <a style={{ textDecoration: "none", color: "white" }} href="tel:123456789">123- (456)-789</a>
+                                        </div>
+                                    </div>
+                                    <div className="d-flex justify-content-center justify-content-lg-start me-4 py-2 py-lg-0">
+                                        <div>
+                                            <svg width="20" height="20" fill="#ffffff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M21.75 3.75H2.25a.75.75 0 0 0-.75.75v15c0 .414.336.75.75.75h19.5a.75.75 0 0 0 .75-.75V4.5a.75.75 0 0 0-.75-.75ZM12 11.622 3.28 7.5h17.44L12 11.622Z"></path>
+                                            </svg>
+                                        </div>
+                                        <div className="location text-white ms-2">
+                                            <a style={{ textDecoration: "none", color: "white" }} href="mailto:isoeasy@gmail.com">isoeasy@gmail.com</a>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                               
+
+                            </Col>
+                        </Row>
+
                     </Container>
                 </div>
             </footer>
