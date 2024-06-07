5932c349e3d983c0066a23e0c4dce3c4810cd72d
muhammadhamza2001 <muhammadhamzackt@gmail.com>
2024-06-07 05:55:00 +0500

    Done

--- src/components/landingpagecomponents/ComingSoonModal.jsx
+++ src/components/landingpagecomponents/ComingSoonModal.jsx
@@ -1,6 +1,8 @@
 // components/SubmissionModal.js
+import Image from 'next/image';
 import React from 'react';
 import { Modal, Button } from 'react-bootstrap';
+import success from "../../assets/success2.jpg"
 
 const ComingSoon = ({ show, handleClose }) => {
     return (
@@ -9,7 +11,14 @@ const ComingSoon = ({ show, handleClose }) => {
 
             </Modal.Header>
             <Modal.Body className='text-center px-4'>
-                Our exciting new product is currently under development. Rest assured, your email and information are securely stored in our records. We will reach out to you soon with more details. Stay tuned!
+                <h3>Thankyou for submitting the form</h3>
+                <div className='pb-3'>
+                <Image src={success} width={100} height={100} alt='success'/>
+                </div>
+               <p>
+               Our exciting new product is currently under development. Rest assured, your email and information are securely stored in our records. We will reach out to you soon with more details. Stay tuned!
+               </p>
+                
             </Modal.Body>
 
         </Modal>
