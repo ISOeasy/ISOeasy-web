6b6eec7a562c4a0ea688264077ef6e61ef1587dd
muhammadhamza2001 <muhammadhamzackt@gmail.com>
2024-06-08 14:18:40 +0500

    Done

--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -1,37 +1,22 @@
 import Link from 'next/link';
 import React from 'react';
-import { Offcanvas, Nav, NavLink } from 'react-bootstrap';
+import { Offcanvas, Nav } from 'react-bootstrap';
 
 const Sidebar = ({ navItems, showOffcanvas, handleOffcanvasClose, activeButton, handleClick }) => {
     return (
-        <Offcanvas show={showOffcanvas} onHide={handleOffcanvasClose} placement="end" className={"Sidebar"}>
+
+
+        <Offcanvas show={showOffcanvas} onHide={handleOffcanvasClose}>
             <Offcanvas.Header closeButton>
                 <Offcanvas.Title>Navigation</Offcanvas.Title>
             </Offcanvas.Header>
-            <Offcanvas.Body className='p-0'>
-          
-                    <div>
-                        {navItems.map((item, index) => (
-                            <NavLink
-                                key={index}
-                                href={item.href}
-                                className={`nav-link-wrapper py-3 ${activeButton === index ? 'activenav' : ''}`}
-                                onClick={() => {
-                                    handleClick(index);
-                                    handleOffcanvasClose();
-                                }}
-                            >
-                                
-                              <span className='px-3'>
-                              {item.label}
-                              </span>
-                            
-                              
-                               
-                            </NavLink>
-                        ))}
-                    </div>
-      
+            <Offcanvas.Body className="p-0">
+                {navItems.map((link, index) => (
+                    <Nav.Link className=" p-3" key={index} href={link.href} onClick={handleClose}>
+                        {link.label}
+                        <div className="underline"></div>
+                    </Nav.Link>
+                ))}
             </Offcanvas.Body>
         </Offcanvas>
     );
