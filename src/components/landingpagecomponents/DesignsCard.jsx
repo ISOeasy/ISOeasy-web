12161e950387f924889390bda53ce0317ce7ea47
muhammadhamza2001 <muhammadhamzackt@gmail.com>
2024-06-10 23:33:02 +0500

    Done

--- src/components/landingpagecomponents/DesignsCard.jsx
+++ src/components/landingpagecomponents/DesignsCard.jsx
@@ -1,9 +1,10 @@
 import Image from 'next/image';
 import React from 'react';
+import { Button, Col, Row } from 'react-bootstrap';
 
 const DesignsCard = ({ image, title, description, onClick }) => {
   return (
-    <div className="projects border-0 " onClick={onClick} style={{cursor:"pointer"}}>
+    <div className="projects border-0 "  >
       <div className="imagewrapper">
         <div style={{ width: '100%', height: '100%', position: 'relative' }}>
           <Image
@@ -11,11 +12,22 @@ const DesignsCard = ({ image, title, description, onClick }) => {
             src={image}
             layout='fill'
             objectFit='cover'
+            onClick={onClick}
+            style={{ cursor: "pointer" }}
           />
         </div>
       </div>
       <div className='py-3 px-2 text-black'>
-        <h5>{title}</h5>
+        <Row>
+          <Col xs={9}>
+            <h5>{title}</h5>
+          </Col>
+          <Col xs={3}>
+          <Button className='w-100 view' onClick={onClick}>View</Button>
+          </Col>
+        </Row>
+
+
         <p>{description}</p>
       </div>
     </div>
